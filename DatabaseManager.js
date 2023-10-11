7aaec4be9b2de725f3747aac00f5b19262ae5a34
xPyth <52961795+xPythh@users.noreply.github.com>
2023-10-11 17:44:12 +0200

    Update DatabaseManager.js
    Implemented new Methods
    Database.close()
    Database.DROP()
    Database.RENAME(string: newName)
    Database.CREATE_TABLE(string: tableName, object: structure, object: config)
    Database.GET_TABLE(string: tableName);
    
    Table.close()
    Table.INSERT(object: values)
    Table.SELECT(object: values)
         .RESULT
         .DELETE()
         .UPDATE_TO(object: values)
    Table.CLEAR();
    Table.DROP()

--- DatabaseManager.js
+++ DatabaseManager.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const crypto = require("crypto");
+
 
 var databases = [];
 
@@ -17,20 +19,17 @@ var isObjectMatch = (obj1, obj2) => {
 class Database 
 {
 	#folder = null;
-	name = null;
+
+	#endThread = false;
 
 	#tables = []
 	get tables() { return this.#tables; }
 
-	getTable(tableName)
+	constructor(dbFolder)
 	{
-		return this.#tables.find(table => table.name === `${tableName}.json`);
-	}
+		if (!/^[a-zA-Z_]+$/.test(dbFolder)) throw new Error(`Database name can only be made out of regular alphabetic characters.`);
 
-	constructor(dbFolder, options = {})
-	{
 		this.#folder = `./${dbFolder}/`;
-		this.name = this.#folder.split("/")[1];
 
 		if (!fs.existsSync(this.#folder))
     		this.#buildDatabase(this.#folder, options)
@@ -44,11 +43,60 @@ class Database
     	}
 	}
 
-	createTable(tableName, structure)
+	close()
 	{
-		if (!/^[a-zA-Z]+$/.test(tableName)) throw new Error(`Table name can only be made out of regular alphabetic characters.`);
+		if (this.#endThread) throw new Error(`Cannot communicate with closed database.`);
+
+		for (var table of this.#tables)
+			table.close();
+
+		this.#endThread = true;
+	}
+
+	DROP()
+	{
+		if (this.#endThread) throw new Error(`Cannot communicate with closed database.`);
+
+		for (var table of this.#tables)
+			table.DROP();
+
+		fs.rmdirSync(this.#folder);
+		this.#endThread = true;
+	}
+
+	RENAME(newDbName)
+	{
+		if (this.#endThread) throw new Error(`Cannot communicate with closed database.`);
+
+		if (!/^[a-zA-Z_]+$/.test(newDbName)) throw new Error(`Database name can only be made out of regular alphabetic characters.`);
+		
+		var tableFiles = this.#tables.map(table => {
+			var name = table.name
+			table.close();
+			return name;
+		});
+
+		fs.renameSync(this.#folder, `./${newDbName}`);
+
+		this.#tables = [];
+		this.#folder = `./${newDbName}/`;
+    	
+    	for (var tableFile of tableFiles)
+    	{
+    		var table = new Table(`${this.#folder}${tableFile}`);
+    		table.load();
+    		this.#tables.push(table);
+    	}
+	}
+
+	CREATE_TABLE(tableName = null, structure = {}, config = {})
+	{
+		if (!/^[a-zA-Z_]+$/.test(tableName)) throw new Error(`Table name can only be made out of regular alphabetic characters.`);
 
 		if (trueTypeOf(structure) !== "object") throw new Error(`Table structure has to be an object.`);
+		if (trueTypeOf(config) !== "object") throw new Error(`Table config has to be an object.`);
+
+		if (this.getTable(tableName)) throw new Error(`Table already exists.`);
 
 		var finalStructure = {};
 
@@ -63,13 +111,24 @@ class Database
 			finalStructure[keyName] = {
 				type: trueTypeOf(type),
 				nullable: properties.includes(null),
+				autoIncr: properties.includes("AUTOINCR")
 			}
+
+			if (finalStructure.autoIncr && type !== "number") throw new Error(`Cannot use autoincrement on ${keyName} wich has type ${type} instead of number`);
 		}
 
-		var table = new Table(`${this.#folder}${tableName}`);
-		table.create(finalStructure);
+		if (config.saveInterval && trueTypeOf(config.saveInterval) !== "number")
+			throw new Error(`Config saveInterval has to be a number`);
+
+		config.saveInterval = (config.saveInterval >= 0) ? config.saveInterval : 100;
+
+
+		var table = new Table(`${this.#folder}${tableName}.json`);
+		table.create(finalStructure, config);
 		this.#tables.push(table);
 	}
+
+	GET_TABLE(tableName) { return this.#tables.find(table => table.name === `${tableName}.json`); }
 
 	#buildDatabase(dbFolder, options)
 	{
@@ -85,9 +144,13 @@ class Table
 	name = null;
 
 	#structure = null;
+	#config = null;
 	#data = [];
 
-	hello = "world";
+	/* Thread Saving System */
+	#dbHash = null;
+	#endThread = false;
+	#saveThread = null;
 
 	constructor(path)
 	{
@@ -95,35 +158,70 @@ class Table
 		this.name = this.#path.split("/")[this.#path.split("/").length-1];
 	}
 
+	// SELF DESTROY [By Database Class]
 	load()
 	{
 		var content = JSON.parse(fs.readFileSync(this.#path, 'utf-8'));
 
 		this.#structure = content.structure;
+		this.#config = content.config;
 		this.#data = content.data;
+
+		this.#autoSave();
+		this.#saveThread = setInterval(() => { this.#autoSave(); }, this.#config.saveInterval);
 
 		delete this.load; // Once one of them are executed, we don't want them accessible
 		delete this.create;
 	}
 
-	#save()
-	{
-		fs.writeFileSync(this.#path, JSON.stringify({ structure: this.#structure, data: this.#data }));
-	}
-
-	create(structure)
+	// SELF DESTROY [By Database Class]
+	create(structure, config)
 	{
 		this.#structure = structure;
+		this.#config = {
+			saveInterval: config.saveInterval,
+			autoIncr: (Object.keys(this.#structure).find(key => this.#structure[key].autoIncr)) ? 0 : -1
+		}
 		this.#data = [];
 		
-		this.#save();
+		this.#saveThread = setInterval(() => { this.#autoSave(); }, this.#config.saveInterval);
 
 		delete this.create;
 		delete this.load;
 	}
 
+	close() { this.#endThread = true; }
+
+	#autoSave()
+	{	
+		var freshHash = crypto.createHash('md5').update(
+			JSON.stringify({ 
+				structure: this.#structure,
+				config: this.#config, 
+				data: this.#data 
+			})
+		).digest('hex');
+
+		if (this.#dbHash === null || this.#dbHash !== freshHash) 
+		{
+			this.#dbHash = freshHash;
+			fs.writeFileSync(this.#path, 
+				JSON.stringify({ 
+					structure: this.#structure,
+					config: this.#config, 
+					data: this.#data 
+				})
+			);
+		}
+
+		if (this.#endThread)
+			clearInterval(this.#saveThread);
+	}
+
 	INSERT(values)
 	{
+		if (this.#endThread) throw new Error(`Cannot communicate with closed table.`);
+
 		if (trueTypeOf(values) !== "object") throw new Error(`Values has to be an object.`);
 
 		var finalEntry = {};
@@ -132,10 +230,19 @@ class Table
 			var structProperties = this.#structure[keyName];
 			var value = values[keyName];
 
+			// Auto Increment
+			if (structProperties.autoIncr)
+			{
+				if (value !== null && value !== undefined) throw new Error(`${keyName} has to be null as an autoincrement`);
+			
+				finalEntry[keyName] = this.#config.autoIncr++;
+				continue;
+			}
 
+			// Nullable
 			if (value === null || value === undefined)
 			{
-				if (!structProperties.nullable) throw new Error(`${keyName} of table ${this.name} cannot be nullable.`);
+				if (!structProperties.nullable) throw new Error(`${keyName} cannot be nullable.`);
 
 				finalEntry[keyName] = null;
 				continue;
@@ -147,11 +254,12 @@ class Table
 			finalEntry[keyName] = value;
 		}
 		this.#data.push(finalEntry);
-		this.#save();
 	}
 
 	SELECT(values)
 	{
+		if (this.#endThread) throw new Error(`Cannot communicate with closed table.`);
+
 		if (trueTypeOf(values) !== "object") throw new Error(`Values has to be an object.`);
 
 		for (var keyName of Object.keys(values))
@@ -160,46 +268,59 @@ class Table
 		var matchingIndexes = [];
 		this.#data.forEach((obj, index) => 
 		{
-		    if (isObjectMatch(obj, values)) {
-		        matchingIndexes.push(index);
-		    }
+		    if (isObjectMatch(obj, values)) 
+		        matchingIndexes.push(index);   
 		});
 
 		return {
-			RESULT: () => { return matchingIndexes.map(index => index = this.#data[index] ); },
+			get RESULT() { 
+				if (this.#endThread) throw new Error(`Cannot communicate with closed table.`);
+				
+				return matchingIndexes.map(index => index = this.#data[index] ); 
+			},
 
 			DELETE: () => 
 			{
+				if (this.#endThread) throw new Error(`Cannot communicate with closed table.`);
+				
 				for (var matchingIndex of matchingIndexes)
 					this.#data.splice(matchingIndex, 1);
-
-				this.#save();
 			},
 			UPDATE_TO: (values) =>
 			{
+				if (this.#endThread) throw new Error(`Cannot communicate with closed table.`);
+				
 				for (var matchingIndex of matchingIndexes)
-				{
 					for (var keyName of Object.keys(values))
+					{
+						if (!this.#structure[keyName]) throw new Error(`Key ${keyName} not present in structure`);
 						this.#data[matchingIndex][keyName] = values[keyName];
-				}
-				this.#save();
+					}
 			}
 		}
 	}
 
 	CLEAR()
 	{
+		if (this.#endThread) throw new Error(`Cannot communicate with closed table.`);
+
 		this.#data = [];
-		this.#save();
+		this.#config.autoIncr = (this.#config.autoIncr === -1) ? -1 : 0;
+	}
+
+	DROP()
+	{
+		if (this.#endThread) throw new Error(`Cannot communicate with closed table.`);
+
+		this.close();
+		fs.unlinkSync(this.#path);
 	}
 }
 
 
 
-module.exports = {
-	databases: databases,
-	
-	load: (dbFolder = null) =>
+module.exports = {	
+	LOAD: (dbFolder = null) =>
 	{
 		if (!dbFolder) throw new Error(`Folder name for the database not provided.`);
 
